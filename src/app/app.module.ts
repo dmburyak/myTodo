ddc4c57d0608a7506dfdb3116ebf169a820da816
Dmitri <d.buryak@gmail.com>
2021-06-10 17:44:08 +0300

    add base code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,96 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+import { SidebarModule } from 'ng-sidebar';
+import { DeviceDetectorModule } from 'ngx-device-detector';
+import { ColorPickerModule } from 'ngx-color-picker';
+
+import { EditCategoryDialogComponent } from './dialog/edit-category-dialog/edit-category-dialog.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { AboutDialogComponent } from './dialog/about/about-dialog.component';
+import { HeaderComponent } from './views/header/header.component';
+import { StatComponent } from './views/stat/stat.component';
+import { StatCardComponent } from './views/stat/stat-card/stat-card.component';
+import { PrioritiesComponent } from './views/priorities/priorities.component';
+import { SettingsDialogComponent } from './dialog/settings-dialog/settings-dialog.component';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditPriorityDialogComponent } from './dialog/edit-priority-dialog/edit-priority-dialog.component';
+import { CategoriesComponent } from './views/categories/categories.component';
+import { TasksComponent } from './views/tasks/tasks.component';
+import { EditTaskDialogComponent } from './dialog/edit-task-dialog/edit-task-dialog.component';
+import { ConfirmDialogComponent } from './dialog/confirm-dialog/confirm-dialog.component';
+import { TaskDatePipe } from './pipe/task-date.pipe';
+
+registerLocaleData(localeRu);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    TasksComponent,
+    EditTaskDialogComponent,
+    ConfirmDialogComponent,
+    TaskDatePipe,
+    EditCategoryDialogComponent,
+    FooterComponent,
+    AboutDialogComponent,
+    HeaderComponent,
+    StatComponent,
+    StatCardComponent,
+    PrioritiesComponent,
+    SettingsDialogComponent,
+    EditPriorityDialogComponent
+
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    ColorPickerModule,
+    SidebarModule,
+    DeviceDetectorModule.forRoot()
   ],
   providers: [],
+  entryComponents: [
+    EditTaskDialogComponent,
+    ConfirmDialogComponent,
+    EditCategoryDialogComponent,
+    AboutDialogComponent,
+    SettingsDialogComponent,
+    EditPriorityDialogComponent
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
